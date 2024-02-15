de0ee1600c61a11e58dfb716351bbe8dcd0625ef
cyrils <nigthguarder@gmail.com>
2024-02-15 23:17:48 +0100

    EJS routing completed.

--- src/main.ts
+++ src/main.ts
@@ -1,26 +1,19 @@
 import express from "express";
 import helmet from "helmet";
 import cors from "cors";
+import path from "path";
+import expressLayouts from "express-ejs-layouts";
 import { MongoConn } from "./services/MongoConnect";
 import errorHandler from "./middleware/exceptions";
-import { PostService } from "./services/PostService";
-import { PostController } from "./controller/PostController";
-import createRouter from "./routes/routes";
+import {appRouter} from "./routes/routes";
 
 //Dotenv
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 require("dotenv").config();
 const PORT = process.env.PORT || 3333;
 const HOST = process.env.HOST || "127.0.0.1";
-
-//Main function...
+//Main.ts
 async function run() {
-  //Express config
-  const app = express();
-  app.use(express.json());
-  app.use(helmet());
-  app.use(cors());
-
   //DB connection
   try {
     await MongoConn.connectDB();
@@ -30,11 +23,38 @@ async function run() {
     await MongoConn.connectDB().finally()
     process.exit(1); //Exiting now
   }
-  app.listen(PORT, () => {
-    console.log(`Express is running on port http://${HOST}:${PORT}.`);
-  });
 
-  //Middleware
-  app.use(errorHandler);
 }
-run();
+run()
+    .then(() => {
+        //Connection to MongoDB was successful
+        const app = express();
+        //Express config
+        app.use(express.json());
+        app.use(helmet());
+        app.use(cors());
+        app.use('/server', (req, res ) => {
+            res.send("Server running...");
+        });
+        //Ejs
+        app.use(expressLayouts);
+        app.set('views', path.join(__dirname, 'views'));
+        app.set('layout', './layouts/layout');
+        app.set('view engine', 'ejs');
+        //Routing
+        app.use(express.static('public'));
+        const ejsRouter = appRouter()
+        app.use("",ejsRouter)
+
+        //Middleware
+        app.use(errorHandler);
+
+        //Console
+      console.log("Server running on port " + PORT);
+      app.listen(PORT, () => {
+            console.log(`Express is running on port http://${HOST}:${PORT}.`);
+        });
+    })
+    .catch((error) => {
+      console.error('Error:', error);
+    });
--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -1,9 +1,22 @@
-import express from "express";
+import express, {Router} from "express";
 
 import { PostController } from "../controller/PostController";
 
 //Pro pouziti vytvor instanci v main.ts..
-export default function createRouter(controller: PostController) {
+
+export function appRouter():Router  {
+  const router = express.Router();
+  router.get('/', (req, res) => {
+    res.render('pages/index', { pageTitle: 'Home' });
+  });
+
+  router.get('/blog', (req, res) => {
+    res.render('pages/blog', {pageTitle: 'Blog'});
+  });
+  return router;
+}
+
+export function createRouter(controller: PostController) {
   const router = express.Router();
 
   //add new post
